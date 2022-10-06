fd6b8c710bacdbd2d2401e909e855af852d0a3bb
Klaas Schoute <klaas_schoute@hotmail.com>
2022-10-06 23:27:34 +0000

    Let users choose there own roles

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 
 def main():
     intents = discord.Intents.default()
+    intents.guilds = True
     intents.message_content = True
     
     activity = discord.Activity(
@@ -28,8 +29,8 @@ def main():
     async def on_message(message: discord.Message):
         username = str(message.author).split('#')[0]
         user_message = str(message.content)
-        channel = str(message.channel.name)
-        print(f'{username} in #{channel}: {user_message}')
+        if message.type == discord.MessageType.default:
+            print(f'{username} in #{message.channel}: {user_message}')
 
         if message.author == bot.user:
             return
@@ -47,10 +48,7 @@ def main():
         bot.load_extension(f'modules.{folder}')
 
     # Run the bot
-    if config.TESTING == "true":
-        bot.run(config.BOT_DEV_TOKEN)
-    else:
-        bot.run(config.BOT_TOKEN)
+    bot.run(config.BOT_TOKEN)
 
 # Run the bot
 if __name__ == "__main__":
