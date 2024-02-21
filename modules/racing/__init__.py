8df74a83ccd955e765580fad26a9d2c3f9cc7753
Klaas Schoute <klaas_schoute@hotmail.com>
2024-02-22 00:45:14 +0100

    Improve module loading function (#152)
    * Improve module loading function
    
    * Small change on cogs

--- modules/racing/__init__.py
+++ modules/racing/__init__.py
@@ -1,40 +1,41 @@
 """Cog for handling racing commands."""
+import discord
 from discord.ext import commands
 
 
 class Racing(commands.Cog, name="Racing"):
-    """Racing class for handling racing commands."""
+    """Specific commands about racing."""
 
     def __init__(self, bot: commands.Bot) -> None:
         """Initialize the Racing class."""
         self.bot = bot
 
-    @commands.command(name="vliegavond")
-    async def trainings(self, ctx: commands.Context) -> None:
-        """Let users know where to find the upcoming flying dates.
+    @commands.command(name="training")
+    async def trainings(self, ctx: discord.ApplicationContext) -> None:
+        """Know where to find the upcoming flying dates.
 
-        command: !vliegavond
+        command: !training
 
         Args:
         ----
-            ctx (commands.Context): The context in which the command was sent.
+            ctx: The context in which the command was sent.
 
         """
-        msg: str = "Een lijst van de aankomende vliegdata is te vinden op: https://dutchdronesquad.nl/trainingen en hier vind je ook uitleg, hoe je kan aanmelden voor een vliegavond."  # noqa: E501
+        msg: str = "Een lijst met aankomende vliegdata is te vinden op: https://dutchdronesquad.nl/trainingen. Daar vind je ook instructies over hoe je je kunt aanmelden voor een vliegavond."  # noqa: E501
         await ctx.send(msg)
 
     @commands.command(name="track")
-    async def track(self, ctx: commands.Context) -> None:
-        """Let users know where to find the current racetrack.
+    async def track(self, ctx: discord.ApplicationContext) -> None:
+        """Know where to find the current racetrack.
 
         command: !track
 
         Args:
         ----
-            ctx (commands.Context): The context in which the command was sent.
+            ctx: The context in which the command was sent.
 
         """
-        msg: str = "Informatie over de huidige racetrack kan je vinden op: https://dutchdronesquad.nl/racetrack, of verken de track alvast in Velocidrone."  # noqa: E501
+        msg: str = "Informatie over de huidige racetrack is te vinden op: https://dutchdronesquad.nl/racetrack. Je kunt ook de track alvast verkennen in Velocidrone."  # noqa: E501
         await ctx.send(msg)
 
 
